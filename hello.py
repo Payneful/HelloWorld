16b1d7af28aae14d15883101f9be8d4855258dc4
Payneful <bradley.payne@familysearch.org>
2022-09-14 14:39:41 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
-import sys, time
+import sys, time, os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def typewriter(string):
     for letter in string:
@@ -6,16 +9,21 @@ def typewriter(string):
         time.sleep(.001)
         print(letter, end="")
 
-typewriter(" _    _    ____    _      _       _____ \n")
-typewriter("| |  | |  |  __|  | |    | |     |  _  |\n")
-typewriter("| |__| |  | |__   | |    | |     | | | |\n")
-typewriter("|  __  |  |  __|  | |    | |     | | | |\n")
-typewriter("| |  | |  | |__   | |__  | |__   | |_| |\n")
-typewriter("|_|  |_|  |____|  |____| |____|  |_____|\n")
-typewriter(" _       _    _____    _____     _       ____     _ \n")
-typewriter("| |  _  | |  |  _  |  | |_| |   | |     |  _ \   | |\n")
-typewriter("| | | | | |  | | | |  |_____|   | |     | | | |  | |\n")
-typewriter("| |_| |_| |  | | | |  | |\ \    | |     | | | |  |_|\n")
-typewriter("|    _    |  | |_| |  | | \ \   | |__   | |_| |   _ \n")
-typewriter("|___| |___|  |_____|  |_|  \_\  |____|  |____/   |_|\n")
-print("\n\n")
+
+def main():
+    clear()
+    typewriter(" _    _    ____    _      _       _____ \n"
+               "| |  | |  |  __|  | |    | |     |  _  |\n"
+               "| |__| |  | |__   | |    | |     | | | |\n"
+               "|  __  |  |  __|  | |    | |     | | | |\n"
+               "| |  | |  | |__   | |__  | |__   | |_| |\n"
+               "|_|  |_|  |____|  |____| |____|  |_____|\n"
+               " _       _    _____    _____     _       ____     _ \n"
+               "| |  _  | |  |  _  |  | |_| |   | |     |  _ \   | |\n"
+               "| | | | | |  | | | |  |_____|   | |     | | | |  | |\n"
+               "| |_| |_| |  | | | |  | |\ \    | |     | | | |  |_|\n"
+               "|    _    |  | |_| |  | | \ \   | |__   | |_| |   _ \n"
+               "|___| |___|  |_____|  |_|  \_\  |____|  |____/   |_|\n\n\n")
+    
+if __name__ == "__main__":
+    main()
